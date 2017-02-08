24bbffed29d4ef8be02e0a21e3c714aa479ee2c5
Topguny <lukasporter@sbcglobal.net>
2017-02-08 12:41:26 -0600

    Created an Xcode project

---
+++ GitXcodeWorkFlow/GitXcodeWorkFlowTests/GitXcodeWorkFlowTests.swift
@@ -0,0 +1,36 @@
+//
+//  GitXcodeWorkFlowTests.swift
+//  GitXcodeWorkFlowTests
+//
+//  Created by Lukas Heading on 2/8/17.
+//  Copyright © 2017 Lukas Heading. All rights reserved.
+//
+
+import XCTest
+@testable import GitXcodeWorkFlow
+
+class GitXcodeWorkFlowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
